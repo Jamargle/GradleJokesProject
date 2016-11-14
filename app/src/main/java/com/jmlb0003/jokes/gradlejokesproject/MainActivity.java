9810691a71d3973494169bf0e4cd5c4c7289a004
Jose Lopez <JoseMiguel.LopezBayona@cognizant.com>
2016-11-14 17:23:41 +0100

    Modify layouts and classes to use butterknife

--- app/src/main/java/com/jmlb0003/jokes/gradlejokesproject/MainActivity.java
+++ app/src/main/java/com/jmlb0003/jokes/gradlejokesproject/MainActivity.java
@@ -2,8 +2,6 @@ package com.jmlb0003.jokes.gradlejokesproject;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,10 +9,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-
-    public void tellJoke(View view) {
-        Toast.makeText(this, "derp", Toast.LENGTH_SHORT).show();
     }
 
 }
