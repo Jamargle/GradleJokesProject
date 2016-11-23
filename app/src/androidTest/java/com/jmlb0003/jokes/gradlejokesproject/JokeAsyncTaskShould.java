c9ada2748e1d547ab98535df465f9fb41f7391bf
Jose Lopez <JoseMiguel.LopezBayona@cognizant.com>
2016-11-23 11:53:19 +0100

    Create some tests for the asynctask

--- app/src/androidTest/java/com/jmlb0003/jokes/gradlejokesproject/JokeAsyncTaskShould.java
+++ app/src/androidTest/java/com/jmlb0003/jokes/gradlejokesproject/JokeAsyncTaskShould.java
@@ -1,7 +1,6 @@
 package com.jmlb0003.jokes.gradlejokesproject;
 
-import android.app.ProgressDialog;
-import android.content.Context;
+import android.app.Instrumentation;
 import android.support.test.annotation.UiThreadTest;
 import android.support.test.rule.UiThreadTestRule;
 import android.support.test.runner.AndroidJUnit4;
@@ -11,7 +10,6 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static android.support.test.InstrumentationRegistry.getTargetContext;
 import static junit.framework.Assert.assertFalse;
 
 /**
@@ -20,50 +18,46 @@ import static junit.framework.Assert.assertFalse;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 @RunWith(AndroidJUnit4.class)
-public class JokeAsyncTaskShould implements JokeAsyncTask.JokeAsyncTaskListener {
+public class JokeAsyncTaskShould extends Instrumentation {
 
     @Rule
     public UiThreadTestRule uiThreadTestRule = new UiThreadTestRule();
 
-    private ProgressDialog dialog;
     private JokeAsyncTask task;
+    private JokeAsyncTask.JokeAsyncTaskListener listener;
 
     @Before
-    public void setUp() throws Throwable {
-        final Context context = getTargetContext();
+    public void setUp() {
+        listener = new JokeAsyncTask.JokeAsyncTaskListener() {
 
-        uiThreadTestRule.runOnUiThread(new Runnable() {
             @Override
-            public void run() {
-                task = new JokeAsyncTask(context, JokeAsyncTaskShould.this);
-                dialog = new ProgressDialog(context);
+            public void showAJoke(final String joke) {
+                assertFalse(joke == null);
+                assertFalse(joke.isEmpty());
             }
-        });
-    }
 
-    @Test
-    @UiThreadTest
-    public void showDialogWhenExecute() {
-        task.execute();
-        dialog.show();
+            @Override
+            public void thereIsNoJokes() {
+
+            }
+
+        };
     }
 
     @Test
     @UiThreadTest
     public void returnsJokeSuccessfully() {
+        task = new JokeAsyncTask(listener);
         task.execute();
-        showAJoke("fakeJoke");
+        listener.showAJoke("fakeJoke");
     }
 
-    @Override
-    public void showAJoke(final String joke) {
-        assertFalse(joke == null);
-        assertFalse(joke.isEmpty());
-    }
-
-    @Override
-    public void thereIsNoJokes() {
-
+    @Test
+    @UiThreadTest
+    public void notReturnsAnyJokesWhenError() {
+        task = new JokeAsyncTask(listener);
+        task.execute();
+        listener.thereIsNoJokes();
     }
 
 }
